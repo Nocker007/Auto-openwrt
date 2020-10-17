82a617df2d73e63ac5356b338fbdeed4f4a1a2e2
Nocker007 <wzr518@foxmail.com>
2020-10-17 11:28:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/luovivi/xmurp-ua.git package/xmurp-ua
+git clone https://github.com/Nocker007/xmurp-ua.git package/xmurp-ua
+git clone https://github.com/muink/luci-app-change-mac.git package/luci-app-change-mac
