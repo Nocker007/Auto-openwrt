ef5dc4d57f2e1e117d45cacbcd3785f369cc22c9
Nocker007 <wzr518@foxmail.com>
2020-10-18 13:12:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,6 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/Nocker007/xmurp-ua.git package/xmurp-ua
-#
 git clone https://github.com/muink/openwrt-rgmac.git package/rgmac
-#
 git clone https://github.com/muink/luci-app-change-mac.git package/luci-app-change-mac
 
