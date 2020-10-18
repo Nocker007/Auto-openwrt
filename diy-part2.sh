c0d1324ce5ad9b8593ee8bc62ab72f29449e18d6
Nocker007 <wzr518@foxmail.com>
2020-10-18 12:18:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#git clone https://github.com/Nocker007/xmurp-ua.git package/xmurp-ua
-git clone https://github.com/muink/openwrt-rgmac.git package/rgmac
-git clone https://github.com/muink/luci-app-change-mac.git package/luci-app-change-mac
+
